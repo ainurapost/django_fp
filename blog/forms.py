c13dff19d4a34774db2cf83eabe2e4ce1dfb63d4
Ainura T <ainurapost@gmail.com>
2021-05-22 20:17:50 +0600

    4

--- blog/forms.py
+++ blog/forms.py
@@ -29,3 +29,7 @@ class RegisterForm(forms.Form):
 class CommentForms(forms.Form):
     # text = forms.Textarea()
     text = forms.CharField(widget=forms.Textarea(attrs=inputAttrs))
+
+class SearchForms(forms.Form):
+    pass
+
